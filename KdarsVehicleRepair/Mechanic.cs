22a1e3f4a312498bc53ef3c06cd27150ee592e1c
KaDarius <32428140+KaDarius@users.noreply.github.com>
2018-02-22 16:13:46 -0600

    Add files via upload
    KdarsVehicleRepair
    Playing around with -accessors and mutators,
    -created a method that calculates information -Getters and setters

---
+++ KdarsVehicleRepair/Mechanic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KdarsVehicleRepair
+{
+    class Mechanic
+    {
+        private string name;
+        private bool onClaim;
+        private double hourlyRate;
+        private double weeklyRate;
+
+        public Mechanic(string Name, bool onClaim, double HourlyRate)
+        {
+            name = Name;
+            onClaim = OnClaim;
+            hourlyRate = HourlyRate;
+            weeklyRate = hourlyRate;
+
+            weeklyRate = PayCalc();
+            PrintReport();
+
+        }
+
+        public string Name { get => name; set => name = value; }
+        public bool OnClaim { get => onClaim; set => onClaim = value; }
+        public double HourlyRate { get => hourlyRate; set => hourlyRate = value; }
+
+        public double PayCalc()
+        {
+            return (40 * HourlyRate);
+        }
+
+        public void PrintReport()
+        {
+          Console.WriteLine("Detective Name: {0:c}", Name);
+          Console.WriteLine("Hourly Rate: {0:c}", HourlyRate);
+          Console.WriteLine("Mechanic on duty?");
+          
+            
+        }
+    }
+}
---
+++ KdarsVehicleRepair/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KdarsVehicleRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string carMake;
+            string mechanicName;
+            string insurance;
+            string caseDescription;
+            string carModel;
+            string carDescription;
+            bool hasInsurance;
+
+            Console.Write("Please input client's name: ");
+            name = Console.ReadLine();
+            Console.Write("Please input the make of the car:");
+            carMake = Console.ReadLine();
+            Console.Write("Please input the model of the car:");
+            carModel = Console.ReadLine();
+            Console.Write("Do you have insurance? (Y/N)");
+            insurance = Console.ReadLine();
+
+            if ( insurance== "Y")
+            {
+                hasInsurance = true;
+
+            }
+            else
+            {
+                hasInsurance = false;
+            }
+            Console.WriteLine(hasInsurance);
+
+            /* bool statment captures whether or  not the user has insurance - possibly add package pricing 
+             * at later date */
+
+            Console.Write("Please input name of mechanic on the job?");
+            mechanicName = Console.ReadLine();
+            Console.Write("Output: Name : {2} make {0} and model {1}:", carMake, carModel, name);
+            Console.ReadKey();
+
+
+           
+        }
+    }
+}
